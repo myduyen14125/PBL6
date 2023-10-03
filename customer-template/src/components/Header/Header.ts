a17df1e67cc45a81eb227b927e0a91dfd9fc29a9
TranNga02 <86636496+TranNga02@users.noreply.github.com>
2023-10-03 23:54:22 +0700

    response header

--- customer-template/src/components/Header/Header.ts
+++ customer-template/src/components/Header/Header.ts
@@ -1,8 +1,10 @@
-import { defineComponent } from "vue";
+import { defineComponent, onMounted } from "vue";
+import SvgIcon from "../BUI/SvgIcon/SvgIcon.vue";
 import logo from "../../assets/image/logo.png";
 
 export default defineComponent({
   name: "Header",
+  component: { SvgIcon },
   setup() {
     const navLinks = [
       {
@@ -14,6 +16,52 @@ export default defineComponent({
         name: "TÌM KIẾM MENTOR",
       },
     ];
-    return { logo, navLinks };
+
+    onMounted(() => {
+      toggleMenu();
+    });
+
+    const toggleMenu = (): void => {
+      const burger = document.querySelectorAll(".navbar-burger");
+      const menu = document.querySelectorAll(".navbar-menu");
+
+      if (burger.length && menu.length) {
+        for (var i = 0; i < burger.length; i++) {
+          burger[i].addEventListener("click", function () {
+            for (var j = 0; j < menu.length; j++) {
+              menu[j].classList.toggle("hidden");
+            }
+          });
+        }
+      }
+
+      // close
+      const close = document.querySelectorAll(".navbar-close");
+      const backdrop = document.querySelectorAll(".navbar-backdrop");
+
+      if (close.length) {
+        for (var i = 0; i < close.length; i++) {
+          close[i].addEventListener("click", function () {
+            for (var j = 0; j < menu.length; j++) {
+              menu[j].classList.toggle("hidden");
+            }
+          });
+        }
+      }
+
+      if (backdrop.length) {
+        for (var i = 0; i < backdrop.length; i++) {
+          backdrop[i].addEventListener("click", function () {
+            for (var j = 0; j < menu.length; j++) {
+              menu[j].classList.toggle("hidden");
+            }
+          });
+        }
+      }
+    };
+    return {
+      logo,
+      navLinks,
+    };
   },
 });
