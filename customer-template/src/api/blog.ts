3a832f2d9dde9186520af814ee18be398ee48e1f
Thoa <thoathoa0110@gmail.com>
2023-10-16 23:47:30 +0700

    feat: create-blog

--- customer-template/src/api/blog.ts
+++ customer-template/src/api/blog.ts
@@ -1,4 +1,10 @@
 import httpRequest from "../ultils/httpRequest";
+import { CreateBlogParams } from "./../types/blog";
+
 
 export const getAllBlogs = async (): Promise<any> =>
   httpRequest.get<any>("/blog");
+
+export const createBlog = async (params: CreateBlogParams): Promise<any> =>
+  httpRequest.post("/blog", params);
+
--- customer-template/src/stores/blog.ts
+++ customer-template/src/stores/blog.ts
@@ -1,6 +1,8 @@
 import { get, noop } from "lodash";
 import { defineStore } from "pinia";
 import { getAllBlogs } from "./../api/blog";
+import { CreateBlogParams } from "../types/blog.js";
+import { createBlog } from "../api/blog.js";
 
 export const useBlog = defineStore("blog", () => {
   const requestGetAllBlogs = async ({
@@ -22,7 +24,29 @@ export const useBlog = defineStore("blog", () => {
     }
   };
 
+  const requestCreateBlog = async ({
+    params,
+    callback,
+  }: {
+    params: CreateBlogParams;
+    callback: App.Callback;
+  }): Promise<void> => {
+    const onSuccess = get(callback, "onSuccess", noop);
+    const onFailure = get(callback, "onFailure", noop);
+    const onFinish = get(callback, "onFinish", noop);
+
+    try {
+      const response = await createBlog(params);
+      onSuccess(response);
+    } catch (error) {
+      onFailure(error);
+    } finally {
+      onFinish();
+    }
+  };
+
   return {
     requestGetAllBlogs,
+    requestCreateBlog,
   };
 });
