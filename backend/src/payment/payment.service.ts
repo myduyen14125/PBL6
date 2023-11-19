f857d25121ab836537e6d010eb1d2dbf9c921c01
Phan Tiến Đạt <127673501+phvntiendat@users.noreply.github.com>
2023-11-19 18:55:29 +0700

    Update payment.service.ts
    Signed-off-by: Phan Tiến Đạt <127673501+phvntiendat@users.noreply.github.com>

--- backend/src/payment/payment.service.ts
+++ backend/src/payment/payment.service.ts
@@ -15,7 +15,7 @@ export class PaymentService {
         var orderId = requestId;
         var orderInfo = "pay with MoMo";
         const redirectUrl = 'https://pbl-6-seven.vercel.app/';
-        const ipnUrl = 'https://pbl6-production.up.railway.app/payment/test;
+        const ipnUrl = 'https://pbl6-production.up.railway.app/payment/test';
         // var ipnUrl = redirectUrl = "https://webhook.site/454e7b77-f177-4ece-8236-ddf1c26ba7f8";
         var amount = payment.amount.toString();
         var requestType = "payWithATM"
