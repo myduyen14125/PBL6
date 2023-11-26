8ac25fa173b41756930797c574591ec781dafbe9
nt-myduyen <myduyen14125@gmail.com>
2023-11-27 02:39:13 +0700

    add cypress

--- customer-template/cypress.config.ts
+++ customer-template/cypress.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  viewportHeight: 720,
+  viewportWidth: 1280,
+  scrollBehavior: false,
   e2e: {
     setupNodeEvents(on, config) {
       baseUrl: "http://localhost:5173";
