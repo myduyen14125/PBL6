54b9bfbf7625022ec5c8eefeeecb388ed5b7b50c
nt-myduyen <myduyen14125@gmail.com>
2023-11-27 02:39:02 +0700

    add validation signup

--- customer-template/src/pages/SignUp/SignUp.ts
+++ customer-template/src/pages/SignUp/SignUp.ts
@@ -38,17 +38,62 @@ export default defineComponent({
     const form = ref<SignUpParams>(initialForm);
     const error = ref(initialError);
     const isSubmitting = ref(false);
+    const isShowPassword = ref(false);
 
     const validateRequired = (fieldName: keyof SignUpParams): string => {
       const err = validate(form.value[fieldName], {
         required: true,
         errorsMessage: { required: "Đây là trường bắt buộc" },
       });
-
       error.value[fieldName] = err;
-
       return err;
     };
+
+    const validateDateOfBirth = (): string => {
+      let err = "";
+      if (!form.value.date_of_birth) {
+        err = "Đây là trường bắt buộc";
+      } else if (form.value.date_of_birth.length !== 10) {
+        err = "Ngày sinh không hợp lệ";
+      } else if (new Date(form.value.date_of_birth).getTime() > new Date().getTime()) {
+        err = "Ngày sinh không được lớn hơn ngày hiện tại";
+      }
+      error.value.date_of_birth = err;
+      return err;
+    }
+
+    const validateEmail = (): string => {
+      let err = "";
+      if (form.value.email === "") {
+        err = "Đây là trường bắt buộc";
+      } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(form.value.email)) {
+        err = "Email không hợp lệ";
+      }
+    
+      error.value.email = err;
+    
+      return err;
+    };    
+
+    const validatePassword = (): string => {
+      let err = "";
+    
+      if (form.value.password === "") {
+        err = "Đây là trường bắt buộc";
+      } else if (form.value.password.length < 6) {
+        err = "Mật khẩu phải có ít nhất 6 ký tự";
+      } else if (form.value.password.length > 20) {
+        err = "Mật khẩu phải có tối đa 20 ký tự";
+      } else if (!/[A-Z]/.test(form.value.password)) {
+        err = "Mật khẩu phải chứa ít nhất 1 chữ cái in hoa";
+      } else if (!/[!@#$%^&*(),.?":{}|<>]/.test(form.value.password)) {
+        err = "Mật khẩu phải chứa ít nhất 1 kí tự đặc biệt";
+      }
+    
+      error.value.password = err;
+    
+      return err;
+    };    
 
     const validateConfirmPassword = (): string => {
       let err = "";
@@ -68,6 +113,12 @@ export default defineComponent({
       arrRes.push(validateRequired("email"));
       arrRes.push(validateRequired("name"));
       arrRes.push(validateRequired("password"));
+      arrRes.push(validateRequired("gender"));
+      arrRes.push(validateRequired("date_of_birth"));
+      arrRes.push(validateRequired("role"));
+      arrRes.push(validateEmail());
+      arrRes.push(validatePassword());
+      arrRes.push(validateDateOfBirth());
       arrRes.push(validateConfirmPassword);
 
       return arrRes.findIndex((x) => x && x.length > 0) < 0;
@@ -83,12 +134,16 @@ export default defineComponent({
         params: form.value,
         callback: {
           onSuccess: (res) => {
+            SwalPopup.swalResultPopup(
+              "Đăng ký tài khoản thành công! Chào mừng bạn đến với IT Mentor!",
+              "sucess"
+            );
             router.push({ name: RouterNameEnum.Home });
             isSubmitting.value = false;
           },
           onFailure: () => {
             SwalPopup.swalResultPopup(
-              "Sorry, looks like there are some errors detected, please try again.",
+              "Người dùng đã tồn tại, vui lòng đăng ký với tài khoản khác!",
               "error"
             );
             isSubmitting.value = false;
@@ -106,7 +161,11 @@ export default defineComponent({
       isSubmitting,
       submitForm,
       validateRequired,
+      validateEmail,
+      validatePassword,
+      validateDateOfBirth,
       validateConfirmPassword,
+      isShowPassword,
     };
   },
 });
