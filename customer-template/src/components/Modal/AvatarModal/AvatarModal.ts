3f6ffa2849e8b96b9e9cde2df84c974195e5d170
nt-myduyen <myduyen14125@gmail.com>
2023-12-09 10:22:22 +0700

    create course

---
+++ customer-template/src/components/Modal/AvatarModal/AvatarModal.ts
@@ -0,0 +1,41 @@
+import { defineComponent, ref } from "vue";
+import SwalPopup from "../../../../ultils/swalPopup";
+import BModal from "../../../../components/BUI/BModal/BModal.vue";
+
+export default defineComponent({
+  name: "AvatarModal",
+  components: { BModal },
+  props: {
+    avatar: {
+      type: String,
+      required: true,
+      default: "",
+    },
+  },
+  emits: ["updated"],
+  setup(props, { emit }) {
+    const modal = ref(false);
+    const isSubmitting = ref(false);
+
+    const show = () => {
+      modal.value = true;
+    };
+
+    const hide = () => {
+      modal.value = false;
+    };
+
+    const updateAvatar = () => {
+      emit("updated", "");
+      hide();
+    };
+
+    return {
+      modal,
+      isSubmitting,
+      show,
+      hide,
+      updateAvatar,
+    };
+  },
+});
